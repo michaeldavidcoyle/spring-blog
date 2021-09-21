35fe7f31f53c56c31117a970631580e917d50c8b
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-21 10:09:49 -0500

    Following class lecture on controllers.

---
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -0,0 +1,41 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    @ResponseBody
+    public String helloFromSpring() {
+        return "Hello from Spring Boot.";
+    }
+
+    @GetMapping("/helloWorld/{username}")
+    @ResponseBody
+    public String helloWorld(@PathVariable String username) {
+        return "Hello, " + username + "!";
+    }
+
+    @GetMapping("/happy_birthday/{username}/{age}")
+    @ResponseBody
+    public String happyBirthday(
+            @PathVariable String username,
+            @PathVariable int age
+    ) {
+        return "Happy Birthday, " + username + "! You are " + age + " years old.";
+    }
+
+    @GetMapping("/random/number")
+    @ResponseBody
+    public int randomNumber() {
+        Random r = new Random();
+        int low = 1;
+        int high = 100;
+        return r.nextInt(high - low + 1) + low;
+    }
+}
