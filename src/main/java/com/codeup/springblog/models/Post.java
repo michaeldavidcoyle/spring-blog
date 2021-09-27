2af4749902b005b08672e6d47f52381e06a6fe6b
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-27 09:20:30 -0500

    Added JPA annotations.

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -1,8 +1,27 @@
 package com.codeup.springblog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
     private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String body;
+
+    public Post() {}
+
+    public Post(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
 
     public Post(String title, String body) {
         this.title = title;
